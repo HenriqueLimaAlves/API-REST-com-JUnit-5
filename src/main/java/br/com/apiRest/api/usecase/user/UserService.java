9996232c93387723b19e1a466596d4ee67b6d7b3
Henrique Lima <henriquelimaalves633@outlook.com>
2022-05-09 23:56:11 -0300

    tratamento de exceção para objeto nao encontrado

--- src/main/java/br/com/apiRest/api/usecase/user/UserService.java
+++ src/main/java/br/com/apiRest/api/usecase/user/UserService.java
@@ -2,6 +2,7 @@ package br.com.apiRest.api.usecase.user;
 
 import br.com.apiRest.api.domain.entities.UserEntity;
 import br.com.apiRest.api.domain.repositories.UserRepository;
+import br.com.apiRest.api.usecase.exceptions.ObjectNotFoundException;
 import br.com.apiRest.api.usecase.user.port.UserPort;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,6 +18,6 @@ public class UserService implements UserPort {
     @Override
     public UserEntity findById(Integer id) {
         Optional<UserEntity> obj = userRepository.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(()-> new ObjectNotFoundException("Objeto não encontrado"));
     }
 }
